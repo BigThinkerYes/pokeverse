bff94678b142f7869979bfa84d569ee8a01f8b94
BigThinkerYes <designaspnet4@hotmail.com>
2022-10-27 09:20:43 -0500

    part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
+import React, { useEffect, useState } from "react";
+import { Navigation } from "./components/Navigation";
+import { PokemonCard } from "./components/PokemonCard";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
-const LIMIT = 150;
-const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
-
+// const LIMIT = 150;
+// const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+// useEffect(() => {
+//   axios
+//     .get(`https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`)
+//     .then(function (response) {
+//       const { data } = response;
+//       const { results } = data;
+//       const newPokeITdata = {};
+//       results.forEach((pokemon, index) => {
+//         newPokeITdata[index + 1] = {
+//           id: index + 1,
+//           name: pokemon.name,
+//           sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+//             index + 1
+//           }.png`,
+//         };
+//       });
+//       setPokemonData(newPokeITdata);
+//     });
+// }, []);
+// };
+//
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState("");
+  const LIMIT = 150;
+  const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+
+  //get api data
+  useEffect(() => {
+    async function fetchData() {
+      // check for responses
+      console.log(response);
+      let response = await getAll(pokeApi);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  //  loading
+  const loadingPokemon = async (data) => {
+    let _poke = await Promise.all(data.map(async (pokemon) => {}));
+  };
 
   return (
     <div data-testid="app">
+      <InputGroup>
+        <InputGroup.Text>Find:</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+      </InputGroup>
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
+      <div>{loading ? <h1>loading...</h1> : <h1>Gotta catch em all</h1>}</div>
       <PokemonCard />
     </div>
   );
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
+import CardGroup from "react-bootstrap/CardGroup";
 
-function PokemonCard({ url, name }) {
+// const PokeIt = (props) => {
+//   const [pokemonData, setPokemonData] = useState({});
+//   const [filter, setFilter] = useState("");
 
-  return (
-    <div>
-        pokemon card
-    </div>
-  );
+export async function PokemonCard({ url, name }) {
+  return new Promise((resolve, reject) => {
+    fetch(url, name)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+  // return (
+  //   <Container>
+  //     <Row>
+  //       <Col xs={12} md={8}>
+  //         <CardGroup>
+  //           <Card>
+  //             {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+  //             <Card.Body>
+  //               <Card.Title>Card title</Card.Title>
+  //               <Card.Text>
+  //                 This is a wider card with supporting text below as a natural
+  //                 lead-in to additional content. This content is a little bit
+  //                 longer.
+  //               </Card.Text>
+  //             </Card.Body>
+  //             <Card.Footer>
+  //               <small className="text-muted">Last updated 3 mins ago</small>
+  //             </Card.Footer>
+  //           </Card>
+  //         </CardGroup>
+  //       </Col>
+  //     </Row>
+  //   </Container>
+  // );
 }
 
-export { PokemonCard };
+// export { PokemonCard };
