c380f6c1fbc4d94b9cf004b1f2defb57dfa314c4
BigThinkerYes <designaspnet4@hotmail.com>
2022-11-07 14:20:04 -0600

    filter works

--- src/App.js
+++ src/App.js
@@ -1,68 +1,115 @@
+import React from "react";
+import { Navigation } from "./components/Navigation";
+import { PokemonCard } from "./components/PokemonCard";
 import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
+import CardGroup from "react-bootstrap/CardGroup";
+import InputGroup from "react-bootstrap/InputGroup";
 import { Navigation } from "./components/Navigation";
 import { PokemonCard } from "./components/PokemonCard";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 
 // const LIMIT = 150;
 // const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
-// useEffect(() => {
-//   axios
-//     .get(`https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`)
-//     .then(function (response) {
-//       const { data } = response;
-//       const { results } = data;
-//       const newPokeITdata = {};
-//       results.forEach((pokemon, index) => {
-//         newPokeITdata[index + 1] = {
-//           id: index + 1,
-//           name: pokemon.name,
-//           sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-//             index + 1
-//           }.png`,
-//         };
-//       });
-//       setPokemonData(newPokeITdata);
-//     });
-// }, []);
-// };
-//
+
 function App() {
-  const [pokemonData, setPokemonData] = useState([]);
-  const [nextUrl, setNextUrl] = useState("");
-  const [prevUrl, setPrevUrl] = useState("");
-  const [loading, setLoading] = useState("");
-  const LIMIT = 150;
-  const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon/?limit=150"
+  );
 
-  //get api data
-  useEffect(() => {
-    async function fetchData() {
-      // check for responses
-      console.log(response);
-      let response = await getAll(pokeApi);
-      setNextUrl(response.next);
-      setPrevUrl(response.previous);
-      await loadingPokemon(response.results);
-      setLoading(false);
-    }
-    fetchData();
-  }, []);
-
-  //  loading
-  const loadingPokemon = async (data) => {
-    let _poke = await Promise.all(data.map(async (pokemon) => {}));
+  const [filterPokemon, setPokemonFilter] = useState([]);
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    const regex = new RegExp(value, "gi");
+    const filtered = allPokemons.filter((sortPokemon) => {
+      return sortPokemon.name.match(regex);
+    });
+    setPokemonFilter(filtered);
   };
 
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+    setLoadMore(data.next);
+
+    function createPokemonObject(results) {
+      results.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+        setAllPokemons((currentList) => [...currentList, data]);
+        console.log(data);
+      });
+    }
+    createPokemonObject(data.results);
+  };
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
   return (
     <div data-testid="app">
-      <InputGroup>
-        <InputGroup.Text>Find:</InputGroup.Text>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
-      <Navigation />
-      <div>{loading ? <h1>loading...</h1> : <h1>Gotta catch em all</h1>}</div>
-      <PokemonCard />
+      <Container>
+        <Row>
+          <Col xs={12} md={12} lg={12}>
+            <div data-testid="app">
+              <Navigation />
+
+              <InputGroup
+                className="mb-3"
+                onChange={(e) => handleSearchChange(e)}
+              >
+                <Button
+                  variant="outline-secondary"
+                  id="button-addon1"
+                  className="load-more"
+                >
+                  Search
+                </Button>
+                <Form.Control
+                  aria-label="search pokemon"
+                  placeholder="search pokemon"
+                  aria-describedby="basic-addon1"
+                />
+              </InputGroup>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          {/* col goes inside map to make images side by side */}
+          {/* {allPokemons.map((pokemon, index) => (
+              <Col>
+              <PokemonCard
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+                key={index}
+              />
+              </Col>
+      
+            ))} */}
+          {filterPokemon.map((pokemon, index) => (
+            <Col key={index}>
+              <PokemonCard
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                type={pokemon.types[0].type.name}
+              />
+            </Col>
+          ))}
+          {/* <h1>Pokemon should appear here</h1> */}
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,49 +1,52 @@
 import React, { useEffect, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/esm/Container";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Modal from "react-bootstrap/Modal";
-import Row from "react-bootstrap/Row";
-import CardGroup from "react-bootstrap/CardGroup";
+import {
+  Button,
+  Card,
+  Container,
+  Modal,
+  Row,
+  Col,
+  CardGroup,
+} from "react-bootstrap";
+// import { AppBar, ToolBar } from "@material-ui/core";
+// const useStyles = makeStyles({
+//   p_container: {
+//     paddingTop: "20px",
+//     paddingLeft: "50px",
+//     paddingRight: "50px",
+//   },
+// });
+const PokemonCard = ({ id, image, type, url, name }) => {
+  // const classes = useStyles();
+  return (
+    <Container>
+      <Row xs={12} md={8} lg={12}>
+        {/* <Col xs={12} md={8} lg={3}> */}
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src={image} alt={name} height={"150"} />
+            <Card.Body>
+              <Card.Title>
+                <h3>{name}</h3>
+              </Card.Title>
+              <Card.Text>
+                <small>
+                  {id} <br />
+                </small>
+                <small>
+                  {type}
+                  <br />
+                </small>
+                <small>{url}</small>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        {/* <div>pokemon card</div> */}
+        {/* </Col> */}
+      </Row>
+    </Container>
+  );
+};
 
-// const PokeIt = (props) => {
-//   const [pokemonData, setPokemonData] = useState({});
-//   const [filter, setFilter] = useState("");
-
-export async function PokemonCard({ url, name }) {
-  return new Promise((resolve, reject) => {
-    fetch(url, name)
-      .then((res) => res.json())
-      .then((data) => {
-        resolve(data);
-      });
-  });
-  // return (
-  //   <Container>
-  //     <Row>
-  //       <Col xs={12} md={8}>
-  //         <CardGroup>
-  //           <Card>
-  //             {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-  //             <Card.Body>
-  //               <Card.Title>Card title</Card.Title>
-  //               <Card.Text>
-  //                 This is a wider card with supporting text below as a natural
-  //                 lead-in to additional content. This content is a little bit
-  //                 longer.
-  //               </Card.Text>
-  //             </Card.Body>
-  //             <Card.Footer>
-  //               <small className="text-muted">Last updated 3 mins ago</small>
-  //             </Card.Footer>
-  //           </Card>
-  //         </CardGroup>
-  //       </Col>
-  //     </Row>
-  //   </Container>
-  // );
-}
-
-// export { PokemonCard };
+export { PokemonCard };
